5a43a134252783ff5eb12deb158479587814fcf0
htwenty-1 <h2ne1.public@gmail.com>
2022-02-27 21:38:22 +0900

    댓글 기능 업데이트

--- SpringBootBackend/src/main/java/com/hwangduil/springbootbackend/controller/ReplyController.java
+++ SpringBootBackend/src/main/java/com/hwangduil/springbootbackend/controller/ReplyController.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
